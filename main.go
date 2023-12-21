633152c50c6ac85598f5f83e8a4c0e2c81d2d09b
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2023-12-22 00:31:41 +0530

    show list on web page
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- main.go
+++ main.go
@@ -7,13 +7,24 @@ import (
 	"text/template"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 
-	h1 := func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
-	}
-	http.HandleFunc("/", h1)
+		films := map[string][]Film {
+			"Films": {
+				{Title: "The Goadfather", Director: "Francis Ford Copppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
+	})
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
